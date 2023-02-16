60f318896e2e59863e8b0bb9e1fbd8475c8c716e
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-02-15 17:08:54 -0800

    Make configs to deploy at cyclic

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
-// Allow to use environment variables
-dotenv.config();
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Movies and Series genres lists
 const moviesGenresList = [
@@ -283,8 +282,6 @@ app.get('/genres/results', (req, res) => {
 });
 
 // Makes app ready to run on production too
-app.listen(process.env.PORT || 3000, () =>
-  console.log('Server running on port 3000')
-);
+app.listen(PORT, () => console.log('Server running on port 3000'));
 
 module.exports = app;
